f1e9f2f20a7881f29610bf0d54b364e91780acb0
vagrant <yunpeople@163.com>
2019-11-26 06:15:27 +0000

    用户控制器大小写问题

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,12 +9,12 @@ class UsersController extends Controller
 {
 	public function create()
 	{
-		return view('Users.create');
+		return view('users.create');
 	}
 
 	public function show(User $user)
 	{
-		return view('Users.show',compact('user'));
+		return view('users.show',compact('user'));
 	}
 
 	public function store(Request $request)
