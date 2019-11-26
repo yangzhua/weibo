7a6622637b4341b7c3f4c968671f33ddcb54d4b1
vagrant <yunpeople@163.com>
2019-11-26 08:00:47 +0000

    访问策略

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -7,6 +7,16 @@ use App\Models\User;
 
 class UsersController extends Controller
 {
+	public function __construct()
+	{
+		$this->middleware('auth',[
+			'except'=>['show','create','update']
+		]);
+		$this->middleware('guest',[
+			'only'=>['create']
+		]);
+	}
+
 	public function create()
 	{
 		return view('users.create');
@@ -39,11 +49,13 @@ class UsersController extends Controller
 
 	public function edit(User $user)
 	{
+		$this->authorize('update', $user);
 		return view('users.edit',compact('user'));
 	}
 
 	public function update(User $user,Request $request)
 	{
+		$this->authorize('update', $user);
 		$this->validate($request, [
 			'name' => 'required|max:50',
 			'password' => 'nullable|confirmed|min:6'
