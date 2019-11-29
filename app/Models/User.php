04c4ab814cc64a6347edbe2ec18568456d18b199
vagrant <yunpeople@163.com>
2019-11-29 11:17:11 +0000

    首页微博列表

--- app/Models/User.php
+++ app/Models/User.php
@@ -56,6 +56,12 @@ class User extends Authenticatable
 	});
     }
 
+    public function feed()
+    {
+    	return $this->statuses()
+                    ->orderBy('created_at', 'desc');
+    }
+
     public function statuses()
     {
     	return $this->hasMany(Status::class);
