ec4f9f1b582147a56117599c08a24cf6a90d909d
yangzhua <1063668731@qq.com>
2022-10-10 05:03:51 +0000

    查看用户列表

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -19,6 +19,12 @@ class UsersController extends Controller
         ]);
     }
 
+    public function index()
+    {
+        $users = User::all();
+        return view('users.index', compact('users'));
+    }
+
     public function create()
     {
         return view('users.create');
